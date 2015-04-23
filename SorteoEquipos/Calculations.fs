16f539628a46d48cc80589ede9cba15fb344ab1c
Claudio Gonzalez Vera <cgonzalezvera@gmail.com>
2015-04-23 20:49:32 -0300

    refactor: i create a new calculations's module

---
+++ SorteoEquipos/Calculations.fs
@@ -0,0 +1,36 @@
+﻿module Calculations
+
+open System
+open System.IO
+
+let rand = new Random(System.DateTime.Now.Day + System.DateTime.Now.Minute)
+
+let AssignWeightToPlayer maxPlayers player = 
+    let weight = rand.Next(1, maxPlayers)
+    weight, player
+
+let EqualizeTeams (team1 : List<int * string>) (team2 : List<int * string>) = 
+    let BalanceNow (teamSmall : List<int * string>) (teamLarge : List<int * string>) = 
+        let rec transferPlayers (small : List<int * string>) (large : List<int * string>) = 
+            if small.Length < large.Length then 
+                let stillSmall = small @ [ large.Head ]
+                let stillLarge = large.Tail
+                let min, may = transferPlayers stillSmall stillLarge
+                min, may
+            else small, large
+        transferPlayers teamSmall teamLarge
+    if team1.Length < team2.Length then 
+        let r1, r2 = BalanceNow team1 team2
+        r1, r2
+    else if team1.Length > team2.Length then 
+        let r1, r2 = BalanceNow team2 team1
+        r1, r2
+    else team1, team2
+
+let FileToList pathFile = 
+    let contentPlayers = File.ReadAllText(pathFile)
+    let players = Array.toList (contentPlayers.Split(',')) |> List.map (fun s -> s.Trim())
+    players
+
+let convertList (list : List<int * string>) = 
+    List.reduce (fun (peso1, name1) (peso2, name2) -> 0, name1 + ", " + name2) list |> (fun (p, n) -> n)
--- SorteoEquipos/Program.fs
+++ SorteoEquipos/Program.fs
@@ -1,38 +1,8 @@
 ﻿//Author: Claudio E.Gonzalez Vera
+
 open System
 open System.IO
-
-let rand = new Random(System.DateTime.Now.Day + System.DateTime.Now.Minute)
-
-let AssignWeightToPlayer maxPlayers player = 
-    let weight = rand.Next(1, maxPlayers)
-    weight, player
-
-let EqualizeTeams (e1 : List<int * string>) (e2 : List<int * string>) = 
-    let BalanceNow (menor : List<int * string>) (mayor : List<int * string>) = 
-        let rec run (menor1 : List<int * string>) (mayor1 : List<int * string>) = 
-            if menor1.Length < mayor1.Length then 
-                let nuevoMenor = menor1 @ [ mayor1.Head ]
-                let nuevoMayor = mayor1.Tail
-                let min, may = run nuevoMenor nuevoMayor
-                min, may
-            else menor1, mayor1
-        run menor mayor
-    if e1.Length < e2.Length then 
-        let r1, r2 = BalanceNow e1 e2
-        r1, r2
-    else if e1.Length > e2.Length then 
-        let r1, r2 = BalanceNow e2 e1
-        r1, r2
-    else e1, e2
-
-let FileToList pathFile = 
-    let contentPlayers = File.ReadAllText(pathFile)
-    let players = Array.toList (contentPlayers.Split(',')) |> List.map (fun s -> s.Trim())
-    players
-
-let convertList (list : List<int * string>) = 
-    List.reduce (fun (peso1, name1) (peso2, name2) -> 0, name1 + ", " + name2) list |> (fun (p, n) -> n)
+open Calculations
 
 // By that function the program start
 [<EntryPoint>]
@@ -44,10 +14,12 @@ let main argv =
     if File.Exists(pathFile) = false then failwith "El archivo de players no se encuentra en el directorio indicado."
     if Directory.Exists(Path.GetDirectoryName(pathResult)) = false then 
         failwith "El directorio de resultados no existe."
-    // ========Algorithm===========
     let players = FileToList pathFile
     let quantityPlayers = List.length players
-    let weighedPlayers = List.map ((AssignWeightToPlayer quantityPlayers)) players |> List.sortBy (fun (value, n) -> value)
+    if quantityPlayers % 2 <> 0 then failwith "La cantidad de jugadores debe ser un numero par."
+    // ========Algorithm===========
+    let weighedPlayers = 
+        List.map ((AssignWeightToPlayer quantityPlayers)) players |> List.sortBy (fun (value, n) -> value)
     let totalWeight, result = weighedPlayers |> List.reduce (fun (peso1, name1) (peso2, name2) -> peso1 + peso2, "")
     let team1, team2 = List.partition (fun (weigh, n) -> weigh >= (totalWeight / quantityPlayers)) weighedPlayers
     let ultimateTeam1, ultimateTeam2 = EqualizeTeams team1 team2
@@ -59,4 +31,4 @@ let main argv =
     printfn "Numero de players: %A" quantityPlayers
     printfn "%A" contentFinal
     File.WriteAllText(pathResult, contentFinal)
-    0 
+    0
